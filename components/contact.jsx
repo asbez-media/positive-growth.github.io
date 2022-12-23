295f774e7b353eab3bfa67dc12ae008544a2d698
Vithushan <63534975+ACuteWoof@users.noreply.github.com>
2022-12-23 19:53:34 +0000

    email contact

--- components/contact.jsx
+++ components/contact.jsx
@@ -1,6 +1,58 @@
 import { Button } from "flowbite-react";
+import { useState } from "react";
+import emailjs from "@emailjs/browser";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
 
 function Contact() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, name, message);
+    emailjs
+      .send(
+        "service_4a3n75o",
+        "template_499y21o",
+        {
+          from_name: name,
+          from_email: email,
+          to_name: "Positive Growth Support",
+          message: message,
+        },
+        "hAW7EJkNMygNslFYB"
+      )
+      .then((result) => {
+        console.log(result.text);
+        toast.success("Message sent successfully!", {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      })
+      .catch((error) => {
+        console.log(error.text);
+        toast.error(
+          "Message failed to send. Try again with different parameters.",
+          {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          }
+        );
+      });
+  };
+
   return (
     <section
       id='contact'
@@ -27,6 +79,7 @@ function Contact() {
               id='email'
               className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
               placeholder='name@server.com'
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
           </div>
@@ -42,6 +95,7 @@ function Contact() {
               id='name'
               className='block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
               placeholder='What should we call you?'
+              onChange={(e) => setName(e.target.value)}
               required
             />
           </div>
@@ -57,11 +111,20 @@ function Contact() {
               rows='6'
               className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
               placeholder='Send us a message and we will get back to you as soon as possible.'
+              onChange={(e) => setMessage(e.target.value)}
             ></textarea>
           </div>
-          <Button>Send message</Button>
+          <Button
+            type='submit'
+            className='w-full'
+            onClick={handleSubmit}
+            disabled={email === "" || name === "" || message === ""}
+          >
+            Send message
+          </Button>
         </form>
       </div>
+      <ToastContainer className={"text-white dark:text-gray-900"} />
     </section>
   );
 }
